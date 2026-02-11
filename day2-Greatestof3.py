1022dcfe7f00b9390e12767290c39c2d514046c6
Surabhi <asurabhisubhaschandra@gmail.com>
2026-02-11 23:17:58 +0530

    Implement program to find the greatest of three numbers

---
+++ day2-Greatestof3.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter the First number"))
+num2=int(input("Enter the Second number"))
+num3=int(input("Enter the Third number"))
+
+if(num1>num2 and num1>num3):
+    print("Number 1 is Greater")
+elif(num2>num1 and num2>num3):
+    print("Number 2 is Greater")
+elif(num3>num1 and num3>num2):
+  print("Number 3 is Greater")
+else:
+  print("Invalid Option")
