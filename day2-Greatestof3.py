3aa299821c054ee1ee539ba492f253b3e93b8b2b
Surabhi <asurabhisubhaschandra@gmail.com>
2026-02-11 23:24:37 +0530

    Update print statements to include number value

--- day2-Greatestof3.py
+++ day2-Greatestof3.py
@@ -3,10 +3,10 @@ num2=int(input("Enter the Second number"))
 num3=int(input("Enter the Third number"))
 
 if(num1>num2 and num1>num3):
-    print("Number 1 is Greater")
+    print(num1,"Number 1 is Greater")
 elif(num2>num1 and num2>num3):
-    print("Number 2 is Greater")
+    print(num2,"Number 2 is Greater")
 elif(num3>num1 and num3>num2):
-  print("Number 3 is Greater")
+  print(num3,"Number 3 is Greater")
 else:
   print("Invalid Option")
