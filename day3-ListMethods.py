76b2465a11ef038cb7ddc18a99fb7331c949a2b4
Surabhi <asurabhisubhaschandra@gmail.com>
2026-02-12 23:20:26 +0530

    Add list methods demonstration in day3-ListMethods.py
    Implement list methods demonstration in Python.

---
+++ day3-ListMethods.py
@@ -0,0 +1,9 @@
+#List Methods using a variable
+
+a=[10,11,12,13,14,15]
+a.append(8) #append adds a new value at end
+a.sort() #sort arranges the elements by default in ascending order
+a.reverse() #reversly arranges the element
+a.insert(2,4) #inserts the value at index,element
+a.pop(1) #deletes the element at the (location)
+print(a) 
