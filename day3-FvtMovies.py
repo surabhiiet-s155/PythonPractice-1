f2cd34248e5e93b4e7d28088833e7289a60adb56
Surabhi <asurabhisubhaschandra@gmail.com>
2026-02-12 23:47:24 +0530

    Add script to collect favorite movies from user

---
+++ day3-FvtMovies.py
@@ -0,0 +1,10 @@
+movie=[]
+
+m1=input("Enter your 1st favorite movie name ")
+m2=input("Enter your 2nd fvt movie name ")
+m3=input("Enter your 3rd fvt movie name ")
+
+movie.append(m1)
+movie.append(m2)
+movie.append(m3)
+print(movie)
