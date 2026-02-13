181b00183bcb5226d174047ff83ec2cb50cebee8
Surabhi <asurabhisubhaschandra@gmail.com>
2026-02-13 23:53:11 +0530

    Create day4-DictMarks.py

---
+++ day4-DictMarks.py
@@ -0,0 +1,11 @@
+marks = {}
+
+math = int(input("Enter Math marks: "))
+science = int(input("Enter Computer Science marks: "))
+english = int(input("Enter English marks: "))
+
+marks["Math"] = math
+marks["CS"] = CS
+marks["English"] = english
+
+print(marks)
